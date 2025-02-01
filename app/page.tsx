67f3491dddf667e4e643b3ed14c061044cec18fe
Henry Catalini Smith <henry@catalinismith.se>
2025-02-01 22:20:58 +0100

    Generate one statistic

--- app/page.tsx
+++ app/page.tsx
@@ -9,31 +9,56 @@ import { Container } from "@arbetsmarknad/components/Container";
 import { HeaderMenu } from "@arbetsmarknad/components/HeaderMenu";
 import { Page } from "@arbetsmarknad/components/Page";
 import { TopLevelHeading } from "@arbetsmarknad/components/TopLevelHeading";
+import sqlite3 from "sqlite3";
 
-export default function Home() {
+async function countDocuments(database: sqlite3.Database): Promise<number> {
+  return new Promise((resolve, reject) => {
+    database.get(
+      `SELECT COUNT(*) as documentCount FROM documents`,
+      (error, row) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(parseInt((row as any).documentCount));
+        }
+      },
+    );
+  });
+}
+
+export default async function Home() {
+  const directoryPath = process.env.SOURCE_DIRECTORY_PATH;
+  const databasePath = `${directoryPath}/db.sqlite`;
+  const database = new sqlite3.Database(databasePath);
+  const documentCount = await countDocuments(database);
   return (
     <Page>
       <HeaderMenu
-        href="https://arbetsmarknad.github.io"
-        text="arbetsmarknad.github.io"
+        href="https://arbetsmiljo.github.io"
+        text="arbetsmiljo.github.io"
       />
       <Breadcrumb className="py-4 w-full flex justify-center">
         <Container>
           <BreadcrumbList>
             <BreadcrumbItem>
-              <BreadcrumbLink href="/">Arbetsmarknad</BreadcrumbLink>
+              <BreadcrumbLink href="/">Arbetsmiljö</BreadcrumbLink>
             </BreadcrumbItem>
             <BreadcrumbSeparator />
             <BreadcrumbItem>
-              <BreadcrumbLink href="/components">Components</BreadcrumbLink>
+              <BreadcrumbLink href={`/${process.env.NEXT_PUBLIC_YEAR}`}>
+                {process.env.NEXT_PUBLIC_YEAR}`
+              </BreadcrumbLink>
             </BreadcrumbItem>
           </BreadcrumbList>
         </Container>
       </Breadcrumb>
       <main className="flex flex-col items-center w-full py-4">
         <Container className="flex flex-col items-start space-y-4">
-          <TopLevelHeading text="Arbetsmarknad Starter Project" />
-          <p>This is a starter project for Arbetsmarknad web content.</p>
+          <TopLevelHeading
+            text={`Arbetsmiljö ${process.env.NEXT_PUBLIC_YEAR}`}
+          />
+          <p>Testing</p>
+          <p>{`Document count: ${documentCount}`}</p>
         </Container>
       </main>
     </Page>
