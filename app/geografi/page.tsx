7f1dcba60da9e01ef31268d57ca64bd0be8330ac
Henry Catalini Smith <henry@catalinismith.se>
2025-02-07 08:23:49 +0000

    Refactor deduplicate

--- app/geografi/page.tsx
+++ app/geografi/page.tsx
@@ -1,15 +1,6 @@
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbSeparator,
-} from "@arbetsmarknad/components/Breadcrumb";
+import { Breadcrumbs } from "@arbetsmarknad/components/Breadcrumb";
 import { Container } from "@arbetsmarknad/components/Container";
-import { HeaderMenu } from "@arbetsmarknad/components/HeaderMenu";
-import { Footer } from "@arbetsmarknad/components/Footer";
 import { Main } from "@arbetsmarknad/components/Main";
-import { Page } from "@arbetsmarknad/components/Page";
 import { TopLevelHeading } from "@arbetsmarknad/components/TopLevelHeading";
 import { DateRangeBarChart } from "@/components/DateRangeBarChart";
 import { Metadata } from "next";
@@ -32,36 +23,15 @@ export default async function Inspections() {
   );
 
   return (
-    <Page>
-      <HeaderMenu
-        href="https://arbetsmiljo.github.io"
-        text="arbetsmiljo.github.io"
-      />
-      <Breadcrumb>
-        <BreadcrumbList>
-          <BreadcrumbItem>
-            <BreadcrumbLink href="https://arbetsmarknad.github.io/">
-              Arbetsmarknad
-            </BreadcrumbLink>
-          </BreadcrumbItem>
-          <BreadcrumbSeparator />
-          <BreadcrumbItem>
-            <BreadcrumbLink href="/">Arbetsmiljö</BreadcrumbLink>
-          </BreadcrumbItem>
-          <BreadcrumbSeparator />
-          <BreadcrumbItem>
-            <BreadcrumbLink href={`/${process.env.NEXT_PUBLIC_YEAR}`}>
-              {process.env.NEXT_PUBLIC_YEAR}
-            </BreadcrumbLink>
-          </BreadcrumbItem>
-          <BreadcrumbSeparator />
-          <BreadcrumbItem>
-            <BreadcrumbLink href={`/${process.env.NEXT_PUBLIC_YEAR}/geografi`}>
-              Geografi
-            </BreadcrumbLink>
-          </BreadcrumbItem>
-        </BreadcrumbList>
-      </Breadcrumb>
+    <>
+      <Breadcrumbs>
+        {{
+          "https://arbetsmarknad.github.io/": "Arbetsmarknad",
+          "/": "Arbetsmiljö",
+          [`/${process.env.NEXT_PUBLIC_YEAR}`]: `${process.env.NEXT_PUBLIC_YEAR}`,
+          [`/${process.env.NEXT_PUBLIC_YEAR}/geografi`]: "Geografi",
+        }}
+      </Breadcrumbs>
       <Main>
         <Container className="flex flex-col items-start space-y-4">
           <TopLevelHeading
@@ -75,13 +45,6 @@ export default async function Inspections() {
           />
         </Container>
       </Main>
-      <Footer
-        sourceCode={[
-          `arbetsmiljo/${process.env.NEXT_PUBLIC_YEAR}`,
-          "arbetsmiljo/autoindex",
-          "arbetsmarknad/components",
-        ]}
-      />
-    </Page>
+    </>
   );
 }
