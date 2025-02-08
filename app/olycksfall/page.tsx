a7f7c7d28fdfd7c2aab0f353ada600ac3ae900c8
Henry Catalini Smith <henry@catalinismith.com>
2025-02-08 22:41:32 +0000

    Add accidents

---
+++ app/olycksfall/page.tsx
@@ -0,0 +1,66 @@
+import { startOfYear, endOfYear, format, parseISO } from "date-fns";
+import { Breadcrumbs } from "@arbetsmarknad/components/Breadcrumb";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@arbetsmarknad/components/Card";
+import { Container } from "@arbetsmarknad/components/Container";
+import { Main } from "@arbetsmarknad/components/Main";
+import { TopLevelHeading } from "@arbetsmarknad/components/TopLevelHeading";
+import { DateRangeBarChart } from "@/components/DateRangeBarChart";
+import { Metadata } from "next";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@arbetsmarknad/components/Table";
+import { initKysely, countCasesPerDay } from "@/lib/database";
+
+export async function generateMetadata(): Promise<Metadata> {
+  return {
+    title: `Olycksfall ${process.env.NEXT_PUBLIC_YEAR}`,
+  };
+}
+
+export default async function Accidents() {
+  const directoryPath = process.env.SOURCE_DIRECTORY_PATH;
+  const databasePath = `${directoryPath}/db.sqlite`;
+  const db = initKysely(databasePath);
+  const accidentCasesPerDay = await countCasesPerDay(db, (q) =>
+    q
+      .where("caseName", "like", "%olycksfall%")
+      .where("documentId", "like", "%-1"),
+  );
+
+  return (
+    <>
+      <Breadcrumbs>
+        {{
+          "https://arbetsmarknad.github.io/": "Arbetsmarknad",
+          "/": "Arbetsmiljö",
+          [`/${process.env.NEXT_PUBLIC_YEAR}`]: `${process.env.NEXT_PUBLIC_YEAR}`,
+          [`/${process.env.NEXT_PUBLIC_YEAR}/olycksfall`]: "Olycksfall",
+        }}
+      </Breadcrumbs>
+      <Main>
+        <Container className="flex flex-col items-start space-y-4">
+          <TopLevelHeading
+            text={`Olycksfall ${process.env.NEXT_PUBLIC_YEAR}`}
+          />
+          <DateRangeBarChart
+            title="Ärenden per dag"
+            description="Antal nya ärenden som innehåller ordet 'olycksfall' per dag"
+            data={accidentCasesPerDay}
+            valueLabel="Ärenden"
+          />
+        </Container>
+      </Main>
+    </>
+  );
+}
